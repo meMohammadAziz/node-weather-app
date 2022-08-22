d23aa401b2272aa040cca219cc4657e5fdea0991
meMohammadAziz <mohammadazizramay@gmail.com>
2022-08-22 19:33:55 +0500

    Setup for heroku

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const hbs = require("hbs");
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 const publicDirectoryPath = path.join(__dirname, "../public");
@@ -72,5 +72,5 @@ app.get("*", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log("Server listening on port");
+  console.log(`Server listening on ${port}`);
 });
